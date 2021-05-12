5658dd4c1a149af989ec1488236d20cb33070a12
Abdullah0505 <>
2021-05-12 13:15:20 +0000

    new edits

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-let number = 15
+const number = 15
 let guess = 0
 
-document.getElementById('button').addEventListener('click', random)
+document.getElementById('myAge').addEventListener('click', myFunction)
 
-function random () {
+function myFunction () {
   while (guess != number) {
   guess = prompt('Guess my age, Hint: The number is between 10 and 20')
-  if (guess === number)
-  console.log('You got it right')
+  if (guess == number) {
+  alert('You got it right')
 } else if (guess > number) {
-  console.log('You guessed too big')
+  alert('You guessed too big, please try again')
 } else if (guess < number) {
-  console.log('You guessed too small')
+  alert('You guessed too small, please try again')
 } else {
-  console.log('Invaid Answer')
+  alert('Invaid answer, please try again')
+}
 }
 }
