3e60d18d5b5a2a27d44410dcf181c91c5e43c9e7
Abdullah0505 <>
2021-05-12 14:23:23 +0000

    Revert

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let guess = 0
 document.getElementById('myage').addEventListener('click', myFunction)
 
 function myFunction () {
-  while (guess != number) {
+  while (guess !== number) {
     guess = prompt('Guess my age, Hint: The number is between 10 and 20')
-    if (guess == number) {
+    if (guess === number) {
       alert('You got it right')
     } else if (guess > number) {
       alert('You guessed too big, please try again')
