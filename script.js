4e7af9a13eba931456ed19918e7e09d533fe6f85
Abdullah0505 <>
2021-05-12 14:02:43 +0000

    linter fixes

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const number = 15
 let guess = 0
 
-document.getElementById('myAge').addEventListener('click', myFunction)
+document.getElementById('myage').addEventListener('click', myFunction)
 
 function myFunction () {
-  while (guess != number) {
-  guess = prompt('Guess my age, Hint: The number is between 10 and 20')
-  if (guess == number) {
-  alert('You got it right')
-} else if (guess > number) {
-  alert('You guessed too big, please try again')
-} else if (guess < number) {
-  alert('You guessed too small, please try again')
-} else {
-  alert('Invaid answer, please try again')
-}
-}
+  while (guess !== number) {
+    guess = prompt('Guess my age, Hint: The number is between 10 and 20')
+    if (guess === number) {
+        alert('You got it right')
+    } else if (guess > number) {
+      alert('You guessed too big, please try again')
+    } else if (guess < number) {
+      alert('You guessed too small, please try again')
+    } else {
+      alert('Invaid answer, please try again')
+    }
+  }
 }
